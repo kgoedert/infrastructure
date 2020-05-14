efe03ae39928def7d81d9a56964eee457c3910d9
Kelly Goedert <kelly@substractum.com.br>
2020-05-14 08:07:09 -0300

    added a database for testing

--- postgresql/init-user-db.sh
+++ postgresql/init-user-db.sh
@@ -4,5 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER crm WITH PASSWORD 'crm';
     CREATE DATABASE crm;
+    CREATE DATABASE crm_test;
+    GRANT ALL PRIVILEGES ON DATABASE crm_test TO crm;
     GRANT ALL PRIVILEGES ON DATABASE crm TO crm;
+
 EOSQL
